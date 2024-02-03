0d95830069930e444bf6f51d9f161958df016406
Heinz <113355864+heinzfieldmann@users.noreply.github.com>
2024-02-03 14:23:16 +0100

    Some sysex investigations

--- src/read_sysex.py
+++ src/read_sysex.py
@@ -2,16 +2,29 @@
 # Can we read and parse the sysex-file?
 
 # Opening the binary file in binary mode as rb(read binary)
-f = open("../patches/bass001.syx", mode="rb")
+# Let do it with one file to examine the file structure.
+# We have some metadata about the patches and also the patch data
+# The patch data is stored in 32 consecutive 128 byte chunks.
+# It would be quite easy to map them 
+# I want to put the data in a database. Perhaps etcd?
+# I might be able to do a hash of the 128 byte struct do avaid storing duplicates if I do large scale imports of patches.
+
+sysex_file = open("../patches/bass001.syx", mode="rb")
  
 # Reading file data with read() method
-data = f.read()
+sysexdata = sysex_file.read()
  
-# Knowing the Type of our data
-print(type(data))
- 
-# Printing our byte sequenced data 
-print(data)
- 
+no_bytes=(len(sysexdata))
+
+#Print start of sysex. First item. Should be xf0/240/11110000
+S_start=(sysexdata[0])
+print(S_start)
+#Print end of sysex. Last item. shoud be xf7/247/11111011
+S_stop=(sysexdata[no_bytes-1])
+print(S_stop)
+# MachineID. Yamaha x43/67
+S_ID=(sysexdata[1])
+print(S_ID)
+
 # Closing the opened file
-f.close()
+sysex_file.close()
